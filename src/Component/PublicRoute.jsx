253148858a64dd8797ee6d16d07f5a1680c5d273
Harry Bhati <harendrabhati98@gmail.com>
2026-01-18 16:12:15 +0530

    update Public

--- src/Component/PublicRoute.jsx
+++ src/Component/PublicRoute.jsx
@@ -1,3 +1,48 @@
+// import { Navigate } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+
+// function PublicRoute({ children }) {
+//   const [loading, setLoading] = useState(true);
+//   const [isAuth, setIsAuth] = useState(false);
+//   const [role, setRole] = useState(null);
+
+//   useEffect(() => {
+//     const checkAuth = async () => {
+//       try {
+//         const API_URL = import.meta.env.VITE_API_URL; // ✅ use env variable
+//         const resp = await axios.get(`${API_URL}/checkAuth`, {
+//           withCredentials: true,
+//         });
+
+//         setIsAuth(true);
+//         setRole(resp.data.user.role); // ✅ store role from backend
+//       } catch (err) {
+//         setIsAuth(false);
+//         setRole(null);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     checkAuth();
+//   }, []);
+
+//   if (loading) return <h2>Checking authentication...</h2>;
+
+//   // ❌ Already logged in → block login/signup
+//   if (isAuth) {
+//     if (role === "admin") {
+//       return <Navigate to="/adm" replace />;
+//     }
+//     return <Navigate to="/normal" replace />;
+//   }
+
+//   // ✅ Not logged in → show page
+//   return children;
+// }
+
+// export default PublicRoute;
 import { Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -5,21 +50,17 @@ import axios from "axios";
 function PublicRoute({ children }) {
   const [loading, setLoading] = useState(true);
   const [isAuth, setIsAuth] = useState(false);
-  const [role, setRole] = useState(null);
 
   useEffect(() => {
     const checkAuth = async () => {
-      try {
+       try {
         const API_URL = import.meta.env.VITE_API_URL; // ✅ use env variable
-        const resp = await axios.get(`${API_URL}/checkAuth`, {
-          withCredentials: true,
+       const resp = await axios.get(`${API_URL}/checkAuth`, {
+         withCredentials: true,
         });
-
-        setIsAuth(true);
-        setRole(resp.data.user.role); // ✅ store role from backend
+        setIsAuth(true);   // logged in
       } catch (err) {
-        setIsAuth(false);
-        setRole(null);
+        setIsAuth(false);  // not logged in
       } finally {
         setLoading(false);
       }
@@ -32,9 +73,6 @@ function PublicRoute({ children }) {
 
   // ❌ Already logged in → block login/signup
   if (isAuth) {
-    if (role === "admin") {
-      return <Navigate to="/adm" replace />;
-    }
     return <Navigate to="/normal" replace />;
   }
 
